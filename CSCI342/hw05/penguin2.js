c29904802f20dc3d49dedd229669adadd7fb8f47
Adam <amra@wwu.edu>
2017-11-30 18:23:22 -0800

    not working, but has code I might want to come back to

--- CSCI342/hw05/penguin2.js
+++ CSCI342/hw05/penguin2.js
@@ -9,26 +9,45 @@
 "use strict";
 
 $(document).ready(function(){
-    var penguinNum;
-    var numPenguins = 18;
+    console.log("lets do this");
+    for (var i = 2; i <= 64; i++){
+        $('#penguinNum').append($('<option>', {
+            value: i,
+            text: i.toString()
+        }));
+    }
+    startGame();
+});
+
+function startGame(){
+
+    var penguinNum ;
+    var numPenguins = document.getElementById("penguinNum").value;
+    var numPenguins = 16;
+    console.log($("#penguinNum"));
+    var origStyleSheet = document.styleSheets[0];
 
     var yeti = document.createElement("div");
     yeti.setAttribute("id", "yeti");
-    var yetiNum = getRandomInt(0, numPenguins);
+    // var yetiNum = getRandomInt(0, numPenguins);
+    var yetiNum = 1;
     var penguin;
-    var gameholder = $("#gameholder");
 
     // yeti is placed if penguinNum == 9, then maxNum--
-    var count = 0;
     for (var i = 0; i < numPenguins; i++){
         if (i != yetiNum){
             penguinNum = getRandomInt(1, 9);
-            count++;
-            penguin = document.createElement("div");
-            penguin.setAttribute("class", ("penguin" + penguinNum.toString()));
-            gameholder.append(penguin);
+            $('#gameholder').append($('<img />', {
+                class: ("penguin" + penguinNum.toString()),
+                src: "images/mound_" + penguinNum.toString() + ".png",
+                onmouseover: "toHoverImage(this)"
+            }));
         }else{
-            gameholder.append(yeti);
+            $('#gameholder').append($('<img />', {
+                class: ("yeti"),
+                src: "images/mound_" + getRandomInt(1, 9) + ".png",
+                onmouseover: "toHoverImage(this)"
+            }));
         }
     }
     
@@ -42,40 +61,56 @@ $(document).ready(function(){
     //     );
     // }
 
-    var styleSheet = document.styleSheets[0];
-    var allCSS = 
-    [].slice.call(document.styleSheets)
-        .reduce(function (prev, styleSheet) {
-            if (styleSheet.cssRules) {
-                return prev +
-                    [].slice.call(styleSheet.cssRules)
-                        .reduce(function (prev, cssRule) {
-                            return prev + cssRule.cssText;
-                        }, '');
-            } else {
-                return prev;
-            }
-        }, '');
-    console.log(styleSheet);
-    console.log(allCSS);
+    // var backgroundImage = window.getComputedStyle(
+    //     document.querySelector('.penguin1'), ':active'
+    // ).getPropertyValue('background-image');
+    // console.log(backgroundImage);
     
+    // var penguinName;
+    // var hoverImages = new Array();
+    // var moundImages = new Array();
+    // for (var i = 0; i < 8; i++){
+    //     penguinName = '.penguin' + (i + 1).toString();
+    //     if ($(penguinName).length){
+    //         hoverImages.push(window.getComputedStyle(
+    //             document.querySelector(penguinName), ':hover'
+    //         ).getPropertyValue('background-image'));
+    //         // hoverImages[i] = hoverImages[i].match
+    //         console.log(hoverImages[i]);
+    //         moundImages.push($(penguinName).css('background-image'));
+    //         console.log(moundImages[i]);
+    //     }
+    // } 
+
     var pengString;
     $("div[class*='penguin']").on("click", function(){
         pengString = $(this).attr('class');
         console.log(pengString);
         penguinNum = pengString.match(/\d ?/);
         $(this).css("background-image", "url('images/penguin_" + penguinNum + ".png')");
-        
     }); 
     
     $("#yeti").mousedown(function(){
-        alert("Yaaaarrrr!");
-        
+        setTimeout(function(){
+            alert("Yaaaarrrr!");
+        }, 50);
+        for(var i = 0; i < 8; i++){
+            penguin = $('.penguin' + (i + 1).toString());
+            if (penguin.length){
+                penguinNum = penguin.attr('class')
+                penguin.css("background-image")
+            }
+        }
     });
-});
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-  }
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+function toHoverImage(element){
+    console.log(element);
+}
+
